35a4bf49cbaf4ec8b2280733d4e7295ab494cb75
Anthony Lombardo <anthony86998@gmail.com>
2022-07-22 14:56:41 -0400

    Add helper function to ensure valid player input

--- script.js
+++ script.js
@@ -2,6 +2,13 @@ function capitalizeString(string){
     return string.substr(0, 1).toUpperCase() + string.substr(1);
 }
 
+function validInput(playerInput){
+    if(playerInput == "rock" || playerInput == "paper" || playerInput == "scissors"){
+        return true;
+    }
+    return false;
+}
+
 function getComputerChoice(){
     let choices = Array("Rock", "Paper", "Scissors");
     let choice = choices[Math.floor(Math.random()*choices.length)];
@@ -25,7 +32,10 @@ function game(){
     let computerScore = 0;
     let playerScore = 0;
     for(let i = 0; i < 5; i++){
-        let playerInput = prompt("Enter Rock, Paper, or Scissors");
+        let playerInput = prompt("Enter Rock, Paper, or Scissors").toLowerCase();
+        while(validInput(playerInput) == false){
+            playerInput = prompt("Try again! Enter Rock, Paper, or Scissors").toLowerCase();
+        }
         let result = playRound(playerInput.toLowerCase(), getComputerChoice().toLowerCase());
         if(result == "You Won"){ playerScore += 1; }
         else if(result == "You Lost"){ computerScore += 1;}
